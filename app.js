01a3a96d6afe6f5ff32589e59f6bc7a504af9290
Anish Khari <anishkhari91@gmail.com>
2024-01-25 07:07:28 +0530

    adding another route

--- app.js
+++ app.js
@@ -59,6 +59,20 @@ app.post('/api/users', async (req, res) => {
   }
 });
 
+app.post('/api/users/find',async(req,res)=>{
+  try{
+    const { email }= req.body;
+    const user = await User.findOne({email});
+    if(!user){
+      return res.status(404).json({error:'User not Found.'});
+    }
+    return res.status(200).json({message:'User found'})
+  }
+  catch(err){
+    res.status(500).json({error:'Internal Server Error'});
+  }
+})
+
 app.post('/api/users/findAndVerify', async (req, res) => {
   try {
     const { email, password } = req.body;
