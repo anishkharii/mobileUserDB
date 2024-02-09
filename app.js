1615ec5fe5104e20c39195e8fc18ded0a6a3d04e
Anish Khari <anishkhari91@gmail.com>
2024-02-09 13:14:12 +0530

    using jwt

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const MONGO_URI = process.env.MONGO_URI //|| 'mongodb://127.0.0.1:27017/loginDB';
+const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-mongoose.connect(MONGO_URI)
+mongoose
+  .connect(MONGO_URI)
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((error) => {
-    console.error('MongoDB connection error:', error);
+    console.error("MongoDB connection error:", error);
     process.exit(1);
   });
 
@@ -27,103 +30,142 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
   email: {
-    type:String,
-    required:true
-},
+    type: String,
+    required: true,
+  },
   password: {
-    type:String,
-    required:true
+    type: String,
+    required: true,
   },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
-app.get('/api/users', async (req, res) => {
-  if(req.query.passkey!=='AnishKhari'){
-    return res.status(401).json({error:'Unauthorised'});
+app.get("/api/users/find-all", async (req, res) => {
+  if (req.query.passkey !== "AnishKhari") {
+    return res.status(401).json({ error: "Unauthorised" });
   }
   try {
     const users = await User.find({});
     res.status(200).json(users);
   } catch (err) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-app.post('/api/users', async (req, res) => {
+const authenticationToken = (req, res, next) => {
+  const token =
+    req.headers.authorization && req.headers.authorization.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ error: "Authenticaton Failed" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ error: "Invalid token" });
+    }
+    req.userId = decoded.userId;
+    next();
+  });
+};
+
+app.get("/api/users", authenticationToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.status(200).json({
+      username: user.username,
+      email: user.email,
+    });
+  } catch (err) {
+    console.error("Failed to fetch user data", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.post("/api/users", async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(409).json({ error: 'Email already exists' });
+      return res.status(409).json({ error: "Email already exists" });
     }
 
-    const newUser = new User({ username, email, password });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
 
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRE_IN,
+    });
+
     res.status(201).json({
-      message: 'User saved',
+      message: "User saved",
+      token,
       username: newUser.username,
-      email: newUser.email
+      email: newUser.email,
     });
   } catch (err) {
-    if (err.name === 'ValidationError') {
-      res.status(400).json({ error: 'Validation Error', details: err.message });
+    if (err.name === "ValidationError") {
+      res.status(400).json({ error: "Validation Error", details: err.message });
     } else {
-      res.status(500).json({ error: 'Internal Server Error' });
+      res.status(500).json({ error: "Internal Server Error" });
     }
   }
 });
 
-
-
-app.post('/api/users/find',async(req,res)=>{
-  try{
-    const {email} = req.body;
-    const user = await User.findOne({email});
-    if(!user){
-      return res.status(404).json({message:'User not Found.'});
+app.post("/api/users/find", async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not Found." });
     }
-    return res.status(200).json({message:'User found'})
+    return res.status(200).json({ message: "User found" });
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
   }
-  catch(err){
-    res.status(500).json({error:'Internal Server Error'});
-  }
-})
+});
 
-app.post('/api/users/findAndVerify', async (req, res) => {
+app.post("/api/users/findAndVerify", async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
 
-    if (user.password !== password) {
-      return res.status(401).json({ error: 'Wrong Password' });
+    const matchPassword = await bcrypt.compare(password, user.password);
+    if (!matchPassword) {
+      return res.status(401).json({ error: "Wrong Password" });
     }
-
-    res.status(200).json({ message: 'User found',user});
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRE_IN,
+    });
+    res.status(200).json({ message: "User found", user, token });
   } catch (err) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-app.post('/api/users/forgot-password',async(req,res)=>{
-  try{
-    const {email, password} = req.body;
-    const user = await User.findOne({email});
-    if(!user){
-      res.status(404).json({error:'User not found'});
+app.post("/api/users/forgot-password", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(404).json({ error: "User not found" });
     }
     user.password = password;
     await user.save();
-    return res.status(200).json({message:'User Password Updated Successfully'});
-  }
-  catch(error){
-    res.status(500).json({error:'Internal Server Error'});
+    return res
+      .status(200)
+      .json({ message: "User Password Updated Successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
